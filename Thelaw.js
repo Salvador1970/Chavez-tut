5fd34df2fc9009951cbd54cc68815d1095995d2d
Andy Bobien <Andybobien95@gmail.com>
2018-10-14 20:11:24 +0000

    Initial commit

---
+++ Thelaw.js
@@ -0,0 +1,61 @@
+function Thewar() {
+  alert("You Got It!!");
+}
+ document.getElementsByClassName("mehey"); document.addEventListener("click", Thewar);
+ 
+ 
+ function Elcap() {
+  document.body.style.background = "blue";
+}
+
+Elcap();
+
+
+
+function Changecolor() {
+
+  var wipes = document.getElementById("tego");
+  var clean = wipes.getContext("2d");
+  mainloop();
+
+  
+}
+
+document.addEventListener("DOMContentLoaded", Changecolor);
+
+
+
+function Mateo() {
+  
+}
+
+function Target() {
+  var wipes = document.getElementById("tego");
+  var clean = wipes.getContext("2d");
+  clean.fillStyle = "red";
+  clean.fillRect(240,240,240,240);
+}
+
+
+function mainloop() {
+  Mateo();
+  Target();
+  window.setTimeout(mainloop,1000 / 60);
+  
+}
+
+
+
+var game = {
+  var wipes: undefined
+  var clean: undefined
+};
+
+
+
+
+
+
+
+
+
