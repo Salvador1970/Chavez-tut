7cc06f5aca267721a10b5dec299b93a95cd962a4
Andy Bobien <Andybobien95@gmail.com>
2018-11-04 08:17:09 +0000

    My Second Files

---
+++ The Resume.js
@@ -0,0 +1,55 @@
+function ValidateEmail(Emailid) {
+  if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(blanca.Emailid.value)) {
+    
+    var Barbara = document.getElementsByClassName("lion")[0].value;
+    alert("Thank You" + " " + Barbara  + " " + "I will respond within 24-48 hours!");
+  }
+  else {
+    alert("Invalid Email Address!");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+  
+
+
+ 
+ 
+
+ 
+ 
+  
+
+  
+  
+  
